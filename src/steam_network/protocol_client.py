6d82adf2aaa6d73e55beb449be3e8bd96ad746f4
ABaumher <baumhera912@gmail.com>
2023-12-25 23:59:45 -0500

    Update Token login call to work when the result is Expired, which has finally happened for some users (tokens last ~7 months, which was roughly mid December since initial release).

--- src/steam_network/protocol_client.py
+++ src/steam_network/protocol_client.py
@@ -285,6 +285,8 @@ class ProtocolClient:
             self._auth_lost_handler = auth_lost_handler
         elif result == EResult.AccessDenied:
             return UserActionRequired.InvalidAuthData
+        elif result == EResult.Expired
+            return UserActionRequired.InvalidAuthData
         else:
             logger.warning(f"authenticate_token failed with code: {result}")
             raise translate_error(result)
